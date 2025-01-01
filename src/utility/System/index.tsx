d96f506a831e6f03bde783bf747ea4bfcab6fab6
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-02 02:29:16 +0300

    feat: System utility for monitoring RAM, CPU, disk, and swap usage

---
+++ src/utility/System/index.tsx
@@ -0,0 +1,77 @@
+import { exec } from "node:child_process"
+
+class System {
+  public static ramUsage(): Promise<UsageMetrics> {
+    return this.handleProcesses<UsageMetrics>(
+      "free | grep Mem | awk '{printf \"{\\\"total\\\":%d, \\\"used\\\":%d, \\\"percentage\\\":%.2f}\\n\", $2, $3, $3/$2 * 100.0}'"
+    )
+  }
+
+  public static swapUsage(): Promise<UsageMetrics> {
+    return this.handleProcesses<UsageMetrics>(
+      "free | grep Swap | awk '{printf \"{\"total\":%d, \"used\":%d, \"percentage\":%.2f}\n\", $2, $3, $3/$2 * 100.0}'"
+    )
+  }
+
+  public static cpuUsage(): Promise<UsageMetrics> {
+    return this.handleProcesses<UsageMetrics>(
+      "top -bn1 | grep 'Cpu(s)' | awk '{printf \"{\"total\":100.0, \"used\":%.2f, \"percentage\":%.2f}\n\", $2 + $4, $2 + $4}'"
+    )
+  }
+
+  public static diskUsage(): Promise<UsageMetrics> {
+    return this.handleProcesses<UsageMetrics>(
+      "df --total | grep total | awk '{printf \"{\"total\":\"%s\", \"used\":\"%s\", \"percentage\":%.2f}\n\", $2, $3, ($3/$2)*100}'"
+    )
+  }
+
+  public static getSystemInfo(): Promise<SystemInfo> {
+    return this.handleProcesses<SystemInfo>(
+      "uname -a | awk '{printf \"{\"kernel_name\":\"%s\", \"hostname\":\"%s\", \"kernel_version\":\"%s\", \"os_version\":\"%s\", \"architecture\":\"%s\"}\n\", $1, $2, $3, $4, $NF}'"
+    )
+  }
+
+  private static handleProcesses<ResultType>(
+    command: string
+  ): Promise<ResultType> {
+    return new Promise((resolve, reject) => {
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          reject(`Error executing command: ${error.message}`)
+          return
+        }
+        if (stderr) {
+          reject(`Command stderr: ${stderr}`)
+          return
+        }
+        try {
+          const result = JSON.parse(stdout.trim()) as ResultType
+          resolve(result)
+        } catch (error) {
+          throw new SystemError("JSON parse error")
+        }
+      })
+    })
+  }
+}
+
+export class SystemError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = "SystemError"
+  }
+}
+
+export default System
+
+type UsageMetrics = {
+  total: number
+  used: number
+  percentage: number
+}
+
+type SystemInfo = {
+  kernel: string
+  hostname: string
+  architecture: string
+}
