ee856b8688555a3faeb869dfb6574b7279a6e10b
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-02 01:40:41 +0300

    feat: update ESLint configuration and dependencies for TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,156 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from "@typescript-eslint/parser"
+import tsPlugin from "@typescript-eslint/eslint-plugin"
+import nextPlugin from "@next/eslint-plugin-next"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "@next/next": nextPlugin
+    },
+    rules: {
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-ignore": "allow-with-description"
+        }
+      ],
+      yoda: [
+        "error",
+        "never",
+        {
+          exceptRange: true
+        }
+      ],
+      "sort-imports": [
+        "error",
+        {
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: [
+            "none",
+            "all",
+            "multiple",
+            "single"
+          ]
+        }
+      ],
+      "require-await": "error",
+      "prefer-template": "error",
+      "prefer-exponentiation-operator": "error",
+      "prefer-destructuring": [
+        "error",
+        {
+          array: false,
+          object: true
+        },
+        {
+          enforceForRenamedProperties: false
+        }
+      ],
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-useless-computed-key": "error",
+      "no-useless-catch": "error",
+      "no-unneeded-ternary": "error",
+      "no-script-url": "error",
+      "no-shadow-restricted-names": "error",
+      "no-return-assign": [
+        "error",
+        "always"
+      ],
+      "no-new-wrappers": "error",
+      "no-nonoctal-decimal-escape": "error",
+      "no-param-reassign": "error",
+      "no-lonely-if": "error",
+      "no-lone-blocks": "error",
+      "no-empty": "error",
+      "no-extra-boolean-cast": "error",
+      "no-implicit-coercion": "error",
+      "no-console": "error",
+      "no-alert": "error",
+      "multiline-comment-style": [
+        "warn",
+        "starred-block"
+      ],
+      "max-len": [
+        "error",
+        {
+          code: 80,
+          ignoreComments: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true
+        }
+      ],
+      "max-lines": [
+        "error",
+        {
+          max: 500,
+          skipBlankLines: true,
+          skipComments: true
+        }
+      ],
+      "max-nested-callbacks": [
+        "error",
+        5
+      ],
+      "id-denylist": [
+        "error",
+        "req",
+        "res",
+        "err",
+        "e",
+        "cb",
+        "acc",
+        "curr",
+        "cur"
+      ],
+      eqeqeq: [
+        "error",
+        "always",
+        {
+          null: "ignore"
+        }
+      ],
+      "default-case-last": "error",
+      "default-case": "error",
+      "dot-notation": "warn",
+      "default-param-last": "error",
+      "capitalized-comments": "warn",
+      camelcase: "warn",
+      "use-isnan": "error",
+      "no-unused-private-class-members": "error",
+      "no-unsafe-finally": "error",
+      "no-unsafe-negation": "error",
+      "no-unsafe-optional-chaining": "error",
+      "no-unreachable-loop": "error",
+      "no-unreachable": "error",
+      "no-unmodified-loop-condition": "error",
+      "no-unexpected-multiline": "error",
+      quotes: [
+        "error",
+        "double",
+        {
+          avoidEscape: true
+        }
+      ],
+      "jsx-quotes": [
+        "error",
+        "prefer-double"
+      ],
+      semi: [
+        "error",
+        "never"
+      ]
+    }
+  }
+]
